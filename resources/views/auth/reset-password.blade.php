94314f17dbe44d4f5dacb7d128ccf053a271832a
Thiradej2912 <xtreme.abc1234@gmail.com>
2023-12-08 13:48:55 +0700

    first commit

---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate/animate.css') }}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('vendor/select2/select2.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="{{ asset('images/img-01.png') }}" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" method="POST" action="{{ route('password.store') }}">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+					<span class="login100-form-title">
+						Reset password
+					</span>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" id="email" type="email" name="email" value="{{ $request->email }}" required autofocus autocomplete="email" placeholder="Email">
+                        <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" id="password"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password"
+                            placeholder="Password">
+                        <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+
+                    <div class="wrap-input100 validate-input" data-validate = "Confirm Password is required">
+						<input class="input100" id="password_confirmation"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password"
+                            placeholder="Confirm Password">
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+                            {{ __('Reset Password') }}
+						</button>
+					</div>
+
+					<div class="text-center p-t-136">
+						
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js')"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('vendor/bootstrap/js/popper.js')"></script>
+	<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js')"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('vendor/select2/select2.min.js')"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('vendor/tilt/tilt.jquery.min.js')"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
